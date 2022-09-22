5f1dfe71abd69bc06e8dcccb9405934ddbacf967
Islombek3555 <113655439+Islombek3555@users.noreply.github.com>
2022-09-22 13:38:48 +0300

    Add files via upload

---
+++ Islombek.py
@@ -0,0 +1,8 @@
+import math
+a=int(input("1-sonni kiriting="))
+b=int(input("2-sonni kiriting="))
+c=(a+b)/2
+d=math.sqrt(a*b)
+print(f"o'rta arifmetigi={c} ga teng")
+print(f"o'rta geometrigi={d} ga teng")
+print('Islombek Fazlidinov 8-ATT_21 guruh talabasi')
